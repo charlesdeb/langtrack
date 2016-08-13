d3b01fcff12746dfdf987d6a144e09f6a9e01639
Charles de Bueger <chabekah@gmail.com>
2016-08-13 20:36:34 +0300

    basic startup flow router installed, showing a home

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,15 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+// Import to load these templates
+import '../../ui/layouts/app-body.js';
+
+
+// now for the routes
+FlowRouter.route('/', {
+  name: 'App.home',
+  action() {
+    BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+  },
+});
